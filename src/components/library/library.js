3e05c1d91faf3553531e6ca8e9f8da2898ca8a94
John Phillips <madhatter003@yahoo.com>
2019-02-01 12:35:56 -0700

    installed and connected dev tools to app and used spread operators to set payload data in store

---
+++ src/components/library/library.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as actions from '../../actions';
+
+import LibraryCourse from './libraryCourse';
+
+class Library extends Component {
+
+    componentDidMount() {
+        this.props.fetchCourses()
+    }
+
+    render() {
+        return (
+            <div className="library">
+                <h1 className="library__title">Course Library</h1>
+                <LibraryCourse/>
+                <LibraryCourse/>
+                <LibraryCourse/>
+            </div>
+        )
+    }
+}
+
+export default connect(null, actions)(Library);
